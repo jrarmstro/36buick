6a37ad395e8777243a53f865196bf1797b5cb9b1
Joel Armstrong <mrjoelarmstrong@gmail.com>
2017-10-25 16:49:06 -0400

    Changed deploy-gh-pages.bat to only push to gh-pages when the master branch is the current branch

--- deploy-gh-pages.bat
+++ deploy-gh-pages.bat
@@ -1,1 +1,13 @@
-git subtree push -P public_html origin gh-pages
+@echo off
+
+for /f %%i in ('git symbolic-ref --short HEAD') do set current_branch=%%i
+
+if %current_branch%==master (
+	git subtree push -P public_html origin gh-pages
+)
+
+if not %current_branch%==master (
+	echo Switch to master branch before pushing to gh-pages!
+	echo.
+	pause
+)
