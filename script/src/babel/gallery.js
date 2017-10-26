c0170edce78464a2fc11c0a543cf3b6fb6f1e4c3
Joel Armstrong <mrjoelarmstrong@gmail.com>
2017-10-25 23:43:10 -0400

    Added new gallery pictures; implemented scss; renamed js/css folders to script/style.

---
+++ script/src/babel/gallery.js
@@ -0,0 +1,24 @@
+const img_path = "img/gallery/";
+const thumb_path = img_path + "/thumbs/";
+const img_types = /\.(jpe?g|png|gif)$/;
+
+$.ajax({
+	url: img_path,
+	success: function (data) {
+		$(data).find("a").attr("href", function (i, val) {
+			if (val.match(img_types)) {
+				$("#gallery").append(`
+					<div class="thumbnail col-sm-12 col-md-6 col-lg-4 col-xl-3">
+						<a href="${img_path}${val}" class="thumbnail" target="_blank">
+							<img src="${thumb_path}${val}"
+							     onerror="if (this.src != '${img_path}${val}') this.src='${img_path}${val}';">
+						</a>
+					</div>
+				`)
+			}
+		});
+	},
+	error: function (jqXHR, exception) {
+		console.log(exception, jqXHR);
+	}
+});
