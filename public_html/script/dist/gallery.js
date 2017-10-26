7dc4ebd44c683c3b174d03ccea5ca6c5a75c124a
Joel Armstrong <mrjoelarmstrong@gmail.com>
2017-10-26 03:16:43 -0400

    Converted gallery to masonry grid so that tiles are more responsive. Added masonry dependency (imagesloaded). Added dev jquery.

--- public_html/script/dist/gallery.js
+++ public_html/script/dist/gallery.js
@@ -4,13 +4,24 @@ var img_path = "img/gallery/";
 var thumb_path = img_path + "/thumbs/";
 var img_types = /\.(jpe?g|png|gif)$/;
 
+var $gallery_container = $("#gallery");
+
 $.ajax({
 	url: img_path,
 	success: function success(data) {
 		$(data).find("a").attr("href", function (i, val) {
 			if (val.match(img_types)) {
-				$("#gallery").append("\n\t\t\t\t\t<div class=\"thumbnail col-sm-12 col-md-6 col-lg-4 col-xl-3\">\n\t\t\t\t\t\t<a href=\"" + img_path + val + "\" class=\"thumbnail\" target=\"_blank\">\n\t\t\t\t\t\t\t<img src=\"" + thumb_path + val + "\"\n\t\t\t\t\t\t\t     onerror=\"if (this.src != '" + img_path + val + "') this.src='" + img_path + val + "';\">\n\t\t\t\t\t\t</a>\n\t\t\t\t\t</div>\n\t\t\t\t");
+				$gallery_container.append("\n\t\t\t\t\t<div class=\"masonry-grid-item col-sm-12 col-md-6 col-lg-4 col-xl-3\">\n\t\t\t\t\t\t<a href=\"" + img_path + val + "\" target=\"_blank\">\n\t\t\t\t\t\t\t<img src=\"" + thumb_path + val + "\"\n\t\t\t\t\t\t\t     class=\"thumbnail\"\n\t\t\t\t\t\t\t\t onerror=\"if (this.src != '" + img_path + val + "') this.src='" + img_path + val + "';\">\n\t\t\t\t\t\t</a>\n\t\t\t\t\t</div>\n\t\t\t\t");
 			}
+
+			$gallery_container.imagesLoaded(function () {
+				$gallery_container.masonry({
+					itemSelector: '.masonry-grid-item',
+					columnWidth: '.masonry-grid-sizer',
+					isAnimated: true,
+					transitionDuration: '0.25s'
+				});
+			});
 		});
 	},
 	error: function error(jqXHR, exception) {
--- public_html/script/src/babel/gallery.js
+++ public_html/script/src/babel/gallery.js
@@ -2,20 +2,32 @@ const img_path = "img/gallery/";
 const thumb_path = img_path + "/thumbs/";
 const img_types = /\.(jpe?g|png|gif)$/;
 
+const $gallery_container = $("#gallery");
+
 $.ajax({
 	url: img_path,
 	success: function (data) {
 		$(data).find("a").attr("href", function (i, val) {
 			if (val.match(img_types)) {
-				$("#gallery").append(`
-					<div class="thumbnail col-sm-12 col-md-6 col-lg-4 col-xl-3">
-						<a href="${img_path}${val}" class="thumbnail" target="_blank">
+				$gallery_container.append(`
+					<div class="masonry-grid-item col-sm-12 col-md-6 col-lg-4 col-xl-3">
+						<a href="${img_path}${val}" target="_blank">
 							<img src="${thumb_path}${val}"
-							     onerror="if (this.src != '${img_path}${val}') this.src='${img_path}${val}';">
+							     class="thumbnail"
+								 onerror="if (this.src != '${img_path}${val}') this.src='${img_path}${val}';">
 						</a>
 					</div>
 				`)
 			}
+			
+			$gallery_container.imagesLoaded(function(){
+				$gallery_container.masonry({
+					itemSelector: '.masonry-grid-item',
+					columnWidth: '.masonry-grid-sizer',
+					isAnimated: true,
+					transitionDuration: '0.25s',
+				});
+			});
 		});
 	},
 	error: function (jqXHR, exception) {
